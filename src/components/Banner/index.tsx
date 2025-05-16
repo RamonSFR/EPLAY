efb2784cbb2a40fbb2a6670443b1e21a00cd283d
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-16 00:55:37 -0300

    banner component done

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,13 +7,15 @@ import Button from '../Button'
 const Banner = () => (
   <S.Image style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Tag size='big'>Highlighted game</Tag>
-      <S.Title>Clair Obscur: Expedition 33</S.Title>
-      <S.Prices>
-        from <span>$60</span> <br />
-        to $49.99
-      </S.Prices>
-      <Button type='link' to='/product' title='click here to enjoy this offer'>
+      <Tag size="big">Highlighted game</Tag>
+      <div>
+        <S.Title>Clair Obscur: Expedition 33</S.Title>
+        <S.Prices>
+          from <span>$69.99</span> <br />
+          to $49.99
+        </S.Prices>
+      </div>
+      <Button type="link" to="/product" title="click here to enjoy this offer">
         Check it out
       </Button>
     </div>
