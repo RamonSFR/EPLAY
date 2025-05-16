7f3ebedc625a81f9cd660f844f4986018cd2602d
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-16 00:45:36 -0300

    button import

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,6 +2,7 @@ import * as S from './styles'
 
 import bannerImg from '../../assets/images/expedition33-banner.jpg'
 import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <S.Image style={{ backgroundImage: `url(${bannerImg})` }}>
@@ -12,6 +13,9 @@ const Banner = () => (
         from <span>$60</span> <br />
         to $49.99
       </S.Prices>
+      <Button type='link' to='/product' title='click here to enjoy this offer'>
+        Check it out
+      </Button>
     </div>
   </S.Image>
 )
