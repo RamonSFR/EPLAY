fdbf6a00cb98cdb5dfeb52b3a54c5ed6421758d3
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-17 11:11:40 -0300

    Gallery modal

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,15 +1,19 @@
-import { Item, Items, Action } from './styles'
+import { useState } from 'react'
+
+import * as S from './styles'
 import Section from '../Section'
 
 import playIco from '../../assets/images/icons/play.png'
 import zoomIco from '../../assets/images/icons/zoom.png'
+import closeIco from '../../assets/images/icons/close.png'
 
 import expeditionImage1 from '../../assets/images/gallery/expedition33/image1.jpg'
 import expeditionImage2 from '../../assets/images/gallery/expedition33/image2.jpg'
+import expeditionImage3 from '../../assets/images/gallery/expedition33/image3.jpg'
 import expeditionVideo1 from '../../assets/videos/gallery/expedition33/video1.webm'
 import expeditionVideo2 from '../../assets/videos/gallery/expedition33/video2.webm'
 
-type GalleryItem = {
+interface GalleryItem {
   type: 'image' | 'video'
   url: string
 }
@@ -17,6 +21,10 @@ type GalleryItem = {
 type Props = {
   defaultCover: string
   name: string
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
 }
 
 const mock: GalleryItem[] = [
@@ -29,6 +37,10 @@ const mock: GalleryItem[] = [
     url: expeditionImage2
   },
   {
+    type: 'image',
+    url: expeditionImage3
+  },
+  {
     type: 'video',
     url: expeditionVideo1
   },
@@ -38,7 +50,13 @@ const mock: GalleryItem[] = [
   }
 ]
 
-const Gallery = ({defaultCover}: Props) => {
+const Gallery = ({ defaultCover, name }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
   const getMediaCover = (item: GalleryItem) => {
     if (item.type === 'image') return item.url
     return defaultCover
@@ -49,28 +67,57 @@ const Gallery = ({defaultCover}: Props) => {
     return playIco
   }
 
+  const closeModal = () =>
+    setModal({ isVisible: false, type: 'image', url: '' })
+
   return (
-  <Section title="Gallery" background="black">
-    <div>
-      <Items>
-        {mock.map((media, index) => (
-          <Item key={media.url}>
-            <img
-              src={getMediaCover(media)}
-              alt={`${index + 1} media of the game ${name}`}
-            />
-            <Action>
+    <>
+      <Section title="Gallery" background="black">
+        <S.Items>
+          {mock.map((media, index) => (
+            <S.Item
+              key={media.url}
+              onClick={() => {
+                setModal({ isVisible: true, type: media.type, url: media.url })
+              }}
+            >
               <img
-                src={getMediaIcon(media)}
-                alt={media.type === 'image' ? 'click to zoom image' : 'click to play video'}
+                src={getMediaCover(media)}
+                alt={`${index + 1} media of the game ${name}`}
               />
-            </Action>
-          </Item>
-        ))}
-      </Items>
-    </div>
-  </Section>
-)
+              <S.Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt={
+                    media.type === 'image'
+                      ? 'click to zoom image'
+                      : 'click to play video'
+                  }
+                />
+              </S.Action>
+            </S.Item>
+          ))}
+        </S.Items>
+      </Section>
+      <S.Modal className={modal.isVisible ? 'visible' : ''}>
+        <S.ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={closeIco} onClick={() => closeModal()} alt="close icon" />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </S.ModalContent>
+        <div
+          className="overlay"
+          onClick={() => closeModal()}
+        />
+      </S.Modal>
+    </>
+  )
 }
 
 export default Gallery
--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -18,6 +18,7 @@ export const Action = styled.div`
   justify-content: center;
   align-items: center;
   opacity: 0;
+  transition: opacity 0.5s ease;
 `
 
 export const Item = styled.li`
@@ -34,7 +35,67 @@ export const Item = styled.li`
   &:hover {
     ${Action} {
       opacity: 1;
-      transition: opacity .25s ease;
+      transition: opacity 0.25s ease;
     }
+  }
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: none;
+  align-items: center;
+  justify-content: center;
+
+  &.visible {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+
+export const ModalContent = styled.div`
+  max-width: 960px;
+  position: relative;
+  z-index: 1;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    img {
+      cursor: pointer;
+    }
+  }
+
+  header {
+    display: flex;
+  }
+
+  img, iframe {
+    display: block;
+    max-width: 100%;
+  }
+
+  iframe {
+    width: 100%;
+    height: 480px;
   }
 `
